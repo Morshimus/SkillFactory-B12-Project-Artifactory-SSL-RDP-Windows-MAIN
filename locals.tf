e4a970829f02db4cf1b9522d53895679c8a8c02d
Daniel Dalavurak <morsh110792@yahoo.com>
2023-03-11 13:13:50 +0300

    OK

---
+++ locals.tf
@@ -0,0 +1,15 @@
+locals {
+
+  azure_auth = jsondecode(file("${path.module}/key.json"))
+
+  ansible_template = templatefile(
+    "${path.module}/templates/ansible_inventory_template.tpl",
+    {
+      user     = var.useros,
+      ip       = module.morsh_instance_ya_1.external_ip_address_morsh_server,
+      hostname = module.morsh_instance_ya_1.hostname_morsh_server
+    }
+  )
+
+  loc_path = "${path.module}/"
+}
